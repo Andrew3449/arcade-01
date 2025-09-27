75bd4c4df20764c6452aa9beb273c4bdeb70fdaf
aj <andrewe2@umbc.edu>
2025-09-27 17:17:36 -0400

    added jumping, double jump, and basic movement (will work on levels next)

---
+++ Assets/Scripts/playerMove.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Callbacks;
+using UnityEngine;
+using UnityEngine.TestTools.Constraints;
+
+
+public class Example : MonoBehaviour
+{
+    Rigidbody2D rb;
+    private bool isGrounded = true;
+    private bool isFalling = false;
+    [SerializeField] float speed = 2.0f;
+    [SerializeField] float jumpHeight = 1.0f;
+     private int jumpCount = 0;
+    float hInput; // horizontal input
+
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        hInput = Input.GetAxis("Horizontal");
+        if ((Input.GetButtonDown("Jump") && isGrounded) || (Input.GetButtonDown("Jump") && jumpCount < 2 && isFalling == true))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpHeight);
+            isGrounded = false;
+            jumpCount += 1;
+        }
+        print(jumpCount);
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(hInput * speed, rb.velocity.y);
+        if (rb.velocity.y < 0)
+        {
+            isFalling = true;
+        }
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        isGrounded = true;
+        jumpCount = 0;
+    }
+}
